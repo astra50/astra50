80eb46ccaab4256055c444060a77497f4bf99a21
Konstantin Grachev <me@grachevko.ru>
2021-12-21 12:19:22 +0300

    CRM: Sentry

--- crm/src/index.js
+++ crm/src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as Sentry from '@sentry/browser';
+
+Sentry.init({
+    dsn: "https://90a208b4e6584767a6bac866c6419493@sentry.automagistre.ru/9",
+    environment: process.env.NODE_ENV,
+});
 
 ReactDOM.render(
     <React.StrictMode>
