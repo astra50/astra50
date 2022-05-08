c3b73d7021630b2180ec15d4239bc736a6c94cac
Konstantin Grachev <me@grachevko.ru>
2022-05-09 00:26:02 +0300

    crm: fix warning perPage is out of range

--- crm/src/land/LandList.tsx
+++ crm/src/land/LandList.tsx
@@ -1,4 +1,4 @@
-import {Datagrid, List, ListProps, NumberField, TextField, TextInput} from 'react-admin'
+import {Datagrid, List, ListProps, NumberField, Pagination, PaginationProps, TextField, TextInput} from 'react-admin'
 import {StreetReferenceField, StreetReferenceInput} from '../street/StreetReference'
 
 const filters = [
@@ -6,12 +6,15 @@ const filters = [
     <TextInput source="number,cadastral_number" label="Поиск" alwaysOn/>,
 ]
 
+const LandPagination = (props: PaginationProps) => <Pagination rowsPerPageOptions={[50, 100, 150]} {...props} />
+
 const LandList = (props: ListProps) =>
     <List {...props}
           title="Участки"
           empty={false}
           filters={filters}
           sort={{field: 'number_integer', order: 'ASC'}}
+          pagination={<LandPagination/>}
           perPage={150}
     >
         <Datagrid
