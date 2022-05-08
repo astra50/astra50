64d290d6cd7b34373d417218dd04d6b53596afab
Konstantin Grachev <me@grachevko.ru>
2022-05-09 00:31:56 +0300

    crm: remove sentry

--- crm/src/index.js
+++ crm/src/index.js
@@ -2,12 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as Sentry from '@sentry/browser';
-
-Sentry.init({
-    dsn: "https://90a208b4e6584767a6bac866c6419493@sentry.automagistre.ru/9",
-    environment: process.env.NODE_ENV,
-});
 
 ReactDOM.render(
     <React.StrictMode>
