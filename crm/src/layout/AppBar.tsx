55b245d5fa7d4ad74f99f9f488e7e82b9aeb894b
Konstantin Grachev <me@grachevko.ru>
2022-11-18 21:24:26 +0300

    CRM: Add dark theme button

--- crm/src/layout/AppBar.tsx
+++ crm/src/layout/AppBar.tsx
@@ -1,9 +1,21 @@
-import {AppBar} from 'react-admin'
 import UserMenu from './UserMenu'
+import { defaultTheme, AppBar, ToggleThemeButton } from 'react-admin';
+import { createTheme, Box, Typography } from '@mui/material';
+
+const darkTheme = createTheme({
+    palette: { mode: 'dark' },
+});
 
 export default () => (
     <AppBar
         userMenu={<UserMenu/>}
     >
+        <Box flex="1">
+            <Typography variant="h6" id="react-admin-title"></Typography>
+        </Box>
+        <ToggleThemeButton
+            lightTheme={defaultTheme}
+            darkTheme={darkTheme}
+        />
     </AppBar>
 )
