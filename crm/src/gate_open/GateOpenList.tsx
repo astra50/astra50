0a637ba92590e2b7426265a361d604092b10d351
Konstantin Grachev <me@grachevko.ru>
2023-04-22 14:28:19 +0300

    feat(crm): GateOpenList: add search and filter by source type

--- crm/src/gate_open/GateOpenList.tsx
+++ crm/src/gate_open/GateOpenList.tsx
@@ -1,13 +1,18 @@
-import {Datagrid, DateField, List, TextField} from 'react-admin'
+import {Datagrid, DateField, List, TextField, TextInput} from 'react-admin'
 import {GateReferenceField} from '../gate/GateReference'
-import {GateOpenReasonReferenceField} from '../gate_open_reason/GateOpenReasonReference'
+import {GateOpenReasonReferenceField, GateOpenReasonReferenceInput} from '../gate_open_reason/GateOpenReasonReference'
+
+const filters = [
+    <TextInput source="source" label="Поиск" alwaysOn/>,
+    <GateOpenReasonReferenceInput source="reason_id" label="Тип источника" alwaysOn/>,
+]
 
 const GateOpenList = () => {
     return (
         <List
             title="Журнал открытия ворот"
             sort={{field: 'created_at', order: 'DESC'}}
-
+            filters={filters}
         >
             <Datagrid bulkActionButtons={false}>
                 <GateReferenceField label="Ворота"/>
