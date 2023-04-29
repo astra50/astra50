c7278d6ea41828ee47651c48ff9688f9dc5e72c7
Konstantin Grachev <me@grachevko.ru>
2023-04-29 16:31:55 +0300

    feat(crm): add MemberPaymentShow

--- crm/src/member_payment/index.tsx
+++ crm/src/member_payment/index.tsx
@@ -1,12 +1,14 @@
 import MemberPaymentCreate from './MemberPaymentCreate'
 import MemberPaymentEdit from './MemberPaymentEdit'
 import MemberPaymentList from './MemberPaymentList'
+import MemberPaymentShow from './MemberPaymentShow'
 
 const member_payment = {
     name: 'member_payment',
     list: MemberPaymentList,
     create: MemberPaymentCreate,
     edit: MemberPaymentEdit,
+    show: MemberPaymentShow,
 }
 
 export default member_payment
