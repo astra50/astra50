6be47d69605625ccc200d854bf92e5998e85bcbd
Konstantin Grachev <me@grachevko.ru>
2023-07-26 20:41:10 +0300

    feat(crm): add sentry

--- crm/src/index.tsx
+++ crm/src/index.tsx
@@ -1,6 +1,11 @@
+import * as Sentry from '@sentry/react'
 import {createRoot} from 'react-dom/client'
 import App from './App'
 import './index.css'
+
+Sentry.init({
+    dsn: 'https://2e8513380c034298aaa69222dcc9366d@o4505595929034752.ingest.sentry.io/4505596927279104',
+})
 
 const container = document.getElementById('app')
 const root = createRoot(container!)
