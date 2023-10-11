c6fc1d7a160ef1d251459e8da72be60411b0cc28
Konstantin Grachev <me@grachevko.ru>
2023-10-11 23:01:44 +0300

    fix: calculate_member_payment_balance_by_account_id

---
+++ hasura/migrations/default/1697054276072_member_payment_balance_fix/up.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE FUNCTION public.calculate_member_payment_balance_by_account_id(_id uuid) RETURNS void AS
+$$
+UPDATE member_payment
+   SET balance = sub.balance
+  FROM (SELECT id,
+               (SELECT SUM(sub.amount)
+                  FROM member_payment sub
+                 WHERE sub.account_id = mp.account_id AND sub.paid_at <= mp.paid_at) AS balance
+          FROM member_payment mp
+         WHERE account_id = _id) sub
+ WHERE member_payment.id = sub.id
+   AND (member_payment.balance IS NULL OR member_payment.balance <> sub.balance);
+$$ LANGUAGE sql;
+
+SELECT calculate_member_payment_balance_by_account_id(id)
+  FROM account;
