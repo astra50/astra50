4c89f7386dee32810e026725825e9208504e1101
Konstantin Grachev <me@grachevko.ru>
2024-12-29 17:13:49 +0300

    fix(alice): add support for gate_open_find_person_id

---
+++ hasura/migrations/default/1735481565395_gate_open_find_person_id/up.sql
@@ -0,0 +1,21 @@
+CREATE OR REPLACE FUNCTION gate_open_find_person_id() RETURNS trigger
+    LANGUAGE plpgsql
+AS
+$$
+BEGIN
+    new.person_id = CASE
+                        WHEN (new.reason_id = 'telephone')
+                            THEN (SELECT person_id FROM public.person_phone WHERE phone = new.source)
+                        WHEN (new.reason_id = 'telegram')
+                            THEN (SELECT id FROM public.person WHERE telegram_id = new.source)
+                        WHEN (new.reason_id = 'site')
+                            THEN (SELECT person_id FROM public.person_email WHERE email = new.source)
+                        WHEN (new.reason_id = 'app' OR new.reason_id = 'alice')
+                            THEN (SELECT id FROM public.person WHERE user_id = new.source::uuid)
+                        WHEN (new.reason_id = 'token') THEN (SELECT personal_access_token.person_id
+                                                             FROM public.personal_access_token
+                                                             WHERE id = new.source::uuid) END;
+
+    RETURN new;
+END;
+$$;
